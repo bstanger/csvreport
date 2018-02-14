d91f9bb478d461214270ac0583d9e622e75c8c77
Becca Stanger <beccastanger@gmail.com>
2018-02-13 19:09:42 -0800

    Finish promises for post response

--- server.js
+++ server.js
@@ -1,74 +1,58 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
 const fs = require('fs');
+const Promise = require('bluebird');
+const app = express();
 
-
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'client')));
 app.use('/jquery', express.static(path.join(__dirname + '/node_modules/jquery/dist')));
 app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//   console.log('get');
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+/// HANDLE POST ///////////////////////////
 
 app.post('/', (req, res) => {
-  //console.log(req.body);
-  dataHandler(req.body);
-  res.send(req.body);
+  dataHandler(req.body)
+    .then( () => {
+      res.sendFile(path.join(__dirname + '/csv_report.csv'));
+    }).catch( err => {
+      console.log(err);
+    });
 });
 
 var dataHandler = data => {
-  // Assumption: all fields are included at outset (diff ones won't be added later)
-  createColHeaders(data);
-  createRows(data);
+  return new Promise((resolve, reject) => {
+    // Assumption: all fields are included at outset (diff ones won't be added later)
+    let row = 0;
+    var stringResult = createRows(data, row);
+    fs.writeFile('csv_report.csv', stringResult, (err) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 };
 
-var createRows = data => {
-  console.log('///////////////////////////////');
+var createRows = (data, row) => {
   let dataRow = "";
   for(var key in data){
     if (key !== "children"){
-      dataRow += data[key];
+      dataRow += (row === 0) ? key : data[key];
       dataRow += ',';
     }
   }
   dataRow = dataRow.substring(0, dataRow.length - 1); // Remove last comma
-  dataRow += "\n";
-  fs.appendFile('csv_report.csv', dataRow, (err) => {
-    if(err) {
-      console.log(err);
-    }
-  });
-  console.log(data.children);
-  if(data.children && data.children.length){
+  dataRow += "\n"; // Add new line
+  if (row === 0) {
+    dataRow += createRows(data, ++row);
+  } else if (data.children && data.children.length){
     for(var i = 0; i < data.children.length; i++){
-      console.log(data.children[i]);
-      createRows(data.children[i]);
+      dataRow += createRows(data.children[i], ++row);
     }
-  }
-};
-
-
-var createColHeaders = data => {
-  let colHeaders = ""
-  for(var key in data){
-    if (key !== "children"){
-      colHeaders += key;
-      colHeaders += ',';
-    }
-  }
-  colHeaders = colHeaders.substring(0, colHeaders.length - 1); // Remove last comma
-  colHeaders += "\n";
-  fs.writeFile('csv_report.csv', colHeaders, (err) => {
-    if(err) {
-      console.log(err);
-    }
-  });
+  };
+  return dataRow;
 };
 
 app.listen(3000, '127.0.0.1', () => console.log('Listening at port 3000'));
