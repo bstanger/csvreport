9bdc6d57bbee75488596b048faac74bbcf328b05
Becca Stanger <beccastanger@gmail.com>
2018-02-13 19:18:50 -0800

    Render table from returned CSV

--- client/app.js
+++ client/app.js
@@ -6,7 +6,25 @@ $(document).ready(function(){
       url: 'http://127.0.0.1:3000',
       method: 'POST',
       contentType: 'application/json',
-      data: formData
+      data: formData,
+      success: function(result){
+        renderTable(result);
+      }
     });
   });
 });
+
+var renderTable = (result) => {
+  var $table = "<table id='results-table'></table>";
+  $('#results').append($table);
+  result = result.split('\n');
+  result.forEach(function(row){
+    var $tr = "<tr></tr>";
+    $('#results-table').append($tr);
+    row = row.split(',');
+    row.forEach(function(col){
+      var $td = "<td>" + col + "</td>";
+      $('#results-table tr:last-of-type').append($td);
+    })
+  });
+};
